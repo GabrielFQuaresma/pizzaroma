f2df13f54272d4e4d6d3574e821c392b604de307
GabrielFQuaresma <work.gabriel.quaresma@gmail.com>
2025-05-07 17:26:18 -0300

    Add: Setup BackEnd com inicio da autenticação do login

--- backend/src/main/java/com/pizzaria/pizzaroma/PizzaromaApplication.java
+++ backend/src/main/java/com/pizzaria/pizzaroma/PizzaromaApplication.java
@@ -1,12 +1,13 @@
-package PizzaRoma.demo;
+package com.pizzaria.pizzaroma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PizzaromaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PizzaromaApplication.class, args);
 	}
+
 }
---
+++ backend/src/main/java/com/pizzaria/pizzaroma/entity/Funcionario.java
@@ -0,0 +1,5 @@
+package com.pizzaria.pizzaroma.entity;
+
+public class Funcionario extends Usuario {
+    
+}
